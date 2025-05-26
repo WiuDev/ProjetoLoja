f3043403d04c17fede95c114942ca21c61c1da8e
Willian Eduardo <willianeduardo9877@hotmail.com>
2025-05-26 14:12:30 -0300

    Inicio de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css"
+import Login from "./pages/auth/login";
+import { Routes, Route } from "react-router-dom";
+import Registro from "./pages/auth/registro";
 
-function App () {
-  return <div>Hello World</div>
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path = 'auth'>
+          <Route path = 'login' element = {<Login/>}/>
+          <Route path = 'registro' element = {<Registro/>}/>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App
